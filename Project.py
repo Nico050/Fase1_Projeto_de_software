21a4738f9c2010f260d3d79a09d3cf6a1e7fa0b5
Nícolas de Almeida Souza <146663066+Nico050@users.noreply.github.com>
2025-01-29 18:44:07 -0300

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,17 +1,27 @@
-print("Bem vindo! Antes de utilizar o programa realize o cadastro: ")
-username = input("Digite seu nome de usuário: ")
+def sign_up():
+    print("Bem vindo! Antes de utilizar o programa realize o cadastro: ")
+    u = input("Digite seu nome de usuário: ")
+    return u
 
+username = sign_up()
 
-print(f"{username} BOAS VINDAS AO SEU GERENCIADOR DE TAREFAS! O QUE DESEJA REALIZAR?")
+print(f"{username.upper()} BOAS VINDAS AO SEU GERENCIADOR DE TAREFAS! O QUE DESEJA REALIZAR?")
 
 list_task = []
 
 while True :
     
-    op = int(input("Digite 1 para adicionar uma tarefa\nDigite 2 para remover uma tarefa\nDigite 3 para ver as tarefas\nDigite 0 para sair do programa\n"))
+    op = int(input("Digite 1 para adicionar uma tarefa\nDigite 2 para remover uma tarefa\nDigite 3 para ver as tarefas\nDigite 0 para sair do programa ou alterar o usuário\n"))
     
     if op == 0 :
-        break
+        op2 = int(input("Digite 1 para alterar o usuário do programa\nDigite 2 para sair do programa\nDigite qualquer outro número para permanecer no sistema\n"))
+        if op2 == 1:
+            username = sign_up()
+            print(f"{username.upper()} BOAS VINDAS AO SEU GERENCIADOR DE TAREFAS! O QUE DESEJA REALIZAR?")
+
+        elif op2 == 2:
+            break
+
 
     elif op == 1 :
         type_task = input("Digite o nome da tarefa: ")
@@ -22,10 +32,15 @@ while True :
 
     elif op == 2 :
         remove = input("Digite o nome do item que deseja remover: ")
+
         for i in list_task :
             if remove == i['nome']:
                 print("Tarefa encontrada! Removendo...")
-                list_task.remove(i)
+                if username != i['gerencia']:
+                    print("Você não é o gerente do projeto, não poderá remover essa tarefa")
+
+                else:
+                    list_task.remove(i)
                 break
 
             else:
